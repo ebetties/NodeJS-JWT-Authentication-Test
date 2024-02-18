ec28a333e71c9f1c5da648b0c7aaf05cf9b72000
ebetties <ebetties@uncc.edu>
2024-02-17 23:02:47 -0500

    Brought PBudget code in

---
+++ Desktop/ITISdevelopment/tmp/personal-budget-react/src/LoginPage/Loginpage.js
@@ -0,0 +1,20 @@
+
+function LoginPage() {
+    return (
+      <div className="page-area">
+
+      <article>
+          <h2>Login</h2>
+          <p>
+              Under Construction.<br/>  Login page is coming soon
+          </p>
+      </article>
+
+      
+
+  </div>
+    );
+  }
+  
+  export default LoginPage;
+  
